2da30a81111c2d4f86d97c184f90cf81d4bce985
Martin Kunz <martinkunz@email.cz>
2023-11-20 20:28:36 +0100

    feat(figures): Skip loading the images when `SKIP_PREPARE_IMAGES` is set

--- src/figures/src/prepare-data.jl
+++ src/figures/src/prepare-data.jl
@@ -1,12 +1,14 @@
 using Images, Serialization
 using Unitful
 
+if !isdefined(@__MODULE__, :SKIP_PREPARE_IMAGES)
+    LR_beads = load(joinpath(abspath(dirname(@__FILE__)), "data/SIM_beads_LR.tiff"); verbose=false)
+    LR_beads_sum = dropdims(sum(LR_beads, dims=3); dims=3)
+    LR_beads_sum = scaleminmax(extrema(LR_beads_sum)...).(LR_beads_sum)
+    LR_beads = scaleminmax(extrema(LR_beads)...).(LR_beads)
+    HR_beads = load(joinpath(abspath(dirname(@__FILE__)), "data/SIM_beads_HR.tiff"); verbose=false)
+end
 const LR_beads_Δxy = 30.5u"nm" * 2
-LR_beads = load(joinpath(abspath(dirname(@__FILE__)), "data/SIM_beads_LR.tiff"))
-LR_beads_sum = dropdims(sum(LR_beads, dims=3); dims=3)
-LR_beads_sum = scaleminmax(extrema(LR_beads_sum)...).(LR_beads_sum)
-LR_beads = scaleminmax(extrema(LR_beads)...).(LR_beads)
 const HR_beads_Δxy = 30.5u"nm"
-HR_beads = load(joinpath(abspath(dirname(@__FILE__)), "data/SIM_beads_HR.tiff"))
 
 const IP_params = deserialize(joinpath(abspath(dirname(@__FILE__)), "data/IP_params.ser"))
